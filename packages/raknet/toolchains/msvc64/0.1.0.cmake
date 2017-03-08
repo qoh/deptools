6322ef0a56dd1b70240e80bc919130ecf5a50d15
Johnny <johnny.patterson.jr@gmail.com>
2017-03-08 01:32:36 -0800

    Updated raknet toolchain for msvc64

--- packages/raknet/toolchains/msvc64/0.1.0.cmake
+++ packages/raknet/toolchains/msvc64/0.1.0.cmake
@@ -1,16 +1,19 @@
-set(${PKG_SIMPLE_NAME}_INCLUDE_DIRS ${PKG_INSTALL_DIR}/include)
-set(${PKG_SIMPLE_NAME}_LIBRARIES ${PKG_INSTALL_DIR}/lib/libRakNetLibStatic.lib)
+set(${PKG_SIMPLE_NAME}_INCLUDE_DIRS ${PKG_SOURCE_DIR}/include)
+set(${PKG_SIMPLE_NAME}_LIBRARIES ${PKG_SOURCE_DIR}/Lib/LibStatic/Release/RakNetLibStatic.lib)
+
 
 # Create project entry
 ExternalProject_Add(${PKG_DISPLAY}
 	DOWNLOAD_NAME ${PKG_DL_FILE}
 	URL ${PKG_DL_URI}
 	PREFIX ${PACKAGE_DIR}
-	CMAKE_ARGS
-		-DCMAKE_INSTALL_PREFIX=${PKG_INSTALL_DIR}
+	CONFIGURE_COMMAND cmake -G "${CMAKE_GENERATOR}" .
 		-DRAKNET_ENABLE_SAMPLES=OFF
 		-DRAKNET_ENABLE_DLL=OFF
+	BUILD_COMMAND cmake --build . --target RakNetLibStatic --config Release
 	BUILD_IN_SOURCE 1
+	INSTALL_COMMAND
+		cmake -E copy_directory ${PKG_SOURCE_DIR}/Source ${PKG_SOURCE_DIR}/include/raknet
 	BUILD_BYPRODUCTS
-		${PKG_INSTALL_DIR}/lib/libRakNetLibStatic.lib
+		${PKG_SOURCE_DIR}/Lib/LibStatic/Release/RakNetLibStatic.lib
 )
