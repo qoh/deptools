45cd4424ae5d83e54b671c160d95b79d15cdeac4
Johnny <johnny.patterson.jr@gmail.com>
2017-03-08 02:50:34 -0800

    Updated bullet toolchain for msvc64

--- packages/bullet/toolchains/msvc64/0.1.0.cmake
+++ packages/bullet/toolchains/msvc64/0.1.0.cmake
@@ -1,16 +1,16 @@
-set(${PKG_SIMPLE_NAME}_INCLUDE_DIRS ${PKG_INSTALL_DIR}/include)
+set(${PKG_SIMPLE_NAME}_INCLUDE_DIRS ${PKG_SOURCE_DIR}/include)
 set(${PKG_SIMPLE_NAME}_LIBRARIES
-	${PKG_INSTALL_DIR}/lib/Bullet3Common.lib
-	${PKG_INSTALL_DIR}/lib/Bullet2FileLoader.lib
-	${PKG_INSTALL_DIR}/lib/Bullet3Collision.lib
-	${PKG_INSTALL_DIR}/lib/Bullet3Dynamics.lib
-	${PKG_INSTALL_DIR}/lib/BulletInverseDynamics.lib
-	${PKG_INSTALL_DIR}/lib/LinearMath.lib
-	${PKG_INSTALL_DIR}/lib/Bullet3Geometry.lib
-	${PKG_INSTALL_DIR}/lib/BulletDynamics.lib
-	${PKG_INSTALL_DIR}/lib/BulletCollision.lib
-	${PKG_INSTALL_DIR}/lib/Bullet3OpenCL_clew.lib
-	${PKG_INSTALL_DIR}/lib/BulletSoftBody.lib
+	${PKG_SOURCE_DIR}/lib/Release/Bullet3Common.lib
+	${PKG_SOURCE_DIR}/lib/Release/Bullet2FileLoader.lib
+	${PKG_SOURCE_DIR}/lib/Release/Bullet3Collision.lib
+	${PKG_SOURCE_DIR}/lib/Release/Bullet3Dynamics.lib
+	${PKG_SOURCE_DIR}/lib/Release/BulletInverseDynamics.lib
+	${PKG_SOURCE_DIR}/lib/Release/LinearMath.lib
+	${PKG_SOURCE_DIR}/lib/Release/Bullet3Geometry.lib
+	${PKG_SOURCE_DIR}/lib/Release/BulletDynamics.lib
+	${PKG_SOURCE_DIR}/lib/Release/BulletCollision.lib
+	${PKG_SOURCE_DIR}/lib/Release/Bullet3OpenCL_clew.lib
+	${PKG_SOURCE_DIR}/lib/Release/BulletSoftBody.lib
 )
 
 # Create project entry
@@ -19,12 +19,14 @@ ExternalProject_Add(${PKG_DISPLAY}
 	URL ${PKG_DL_URI}
 	PREFIX ${PACKAGE_DIR}
 	CMAKE_ARGS
-		-DCMAKE_INSTALL_PREFIX=${PKG_INSTALL_DIR}
 		-DUSE_GRAPHICAL_BENCHMARK=OFF
 		-DBUILD_CPU_DEMOS=OFF
 		-DBUILD_BULLET2_DEMOS=OFF
 		-DBUILD_UNIT_TESTS=OFF
 		-DBUILD_EXTRAS=OFF
 		-DUSE_GLUT=OFF
+	BUILD_IN_SOURCE 1
+	INSTALL_COMMAND
+		cmake -E copy_directory ${PKG_SOURCE_DIR}/src ${PKG_SOURCE_DIR}/include/bullet
 	BUILD_BYPRODUCTS ${PKG_SIMPLE_NAME}_LIBRARIES
 )
